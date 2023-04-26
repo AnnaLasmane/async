c30c8c96e9c13f41f0a1028a6ece6f89aa36ae6c
Lasmane <anna.lasmane@accenture.com>
2023-04-26 11:26:08 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'flexboxgrid'
 
-function App() {
+
+const App = () => {
+  const [activeIndex, setActiveIndex] = useState(1)
+  const [character, setCharacter] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`https://rickandmortyapi.com/api/character/${activeIndex}`)
+    .then((res) => {
+      setCharacter(res.data)
+      setLoading(false)
+    })
+  }, [activeIndex])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+      <div className="col-xs-12">
+       {loading && <h1> Loading...</h1>}
+       {!loading && character && (
+        <div>
+          <img src={character.image} alt="" height="100"/>
+          <h3>
+            Name: {character.name}
+          </h3>
+          <h5>
+            Species: {character.species}
+          </h5>
+          <div>
+            <button onClick={() => setActiveIndex(activeIndex -1)}
+            disabled={activeIndex <= 1}
+            >
+            Previous
+            </button>
+            <button onClick={() => setActiveIndex(activeIndex +1)}>
+              Next
+            </button>
+          </div>
+        </div>
+       )}
+       
     </div>
-  );
-}
-
+    </div>
+    </div>
+    );
+    }
+  
 export default App;
